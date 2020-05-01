f290dda76b8c566b6c8f62602ef6ed9620cdef23
irixoc <irixoc@gmail.com>
2020-05-01 15:32:25 -0400

    finished first iteration of jorg functionality, maybe I'll let the user name the file in the next iteration

---
+++ jorg.go
@@ -0,0 +1,32 @@
+// Package jorg is used indent json files for readability
+package jorg
+
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+)
+
+// IndentJSON reads in a byteslice of a json object and prints it to file
+func IndentJSON(byteSlice []byte) error {
+
+	var buffer bytes.Buffer
+	json.Indent(&buffer, byteSlice, "", "   ")
+
+	file, err := os.Create("indented.json")
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(buffer.Bytes())
+	if err != nil {
+		file.Close()
+		return err
+	}
+
+	if err := file.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
